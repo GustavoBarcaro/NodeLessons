7732cb8f3206ea9efc37550fe5eeb914dd6c90a9
Gyubido <52919597+GustavoBarcaro@users.noreply.github.com>
2020-03-14 17:39:15 -0300

    nodemon

--- myFirst_app/routes.js
+++ myFirst_app/routes.js
@@ -15,25 +15,18 @@ const requestHandler =  (req, res) => {
     if(url === '/message' && method === 'POST'){
         const body = [];
         req.on('data', chunk => {
-            console.log(chunk)
             body.push(chunk);
-        });
-        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
+            res.write('<html>');
+            res.write('<head><title> My App</title></head>');
+            res.write('<body><h1>Hello world </h1>');
+            res.write(`<p>${message}</p>`);
+            res.write('</body></html>');
+            res.end();
         });
+        
     }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> My App</title></head>');
-    res.write('<body><h1>Hello world</h1></body>')
-    res.write('</html>');
-    res.end();
 }; 
 
 module.exports = {
