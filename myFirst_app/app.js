bc06c8649c2eceac74ab63e139ff78d248791e2d
Gyubido <gubarcaro@gmail.com>
2020-06-24 12:08:38 -0300

    without node_modelus

--- myFirst_app/app.js
+++ myFirst_app/app.js
@@ -2,21 +2,25 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('handlebars', expressHbs());
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', expressHbs({ 
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout'
+// }));
+// app.set('view engine', 'handlebars');
 // app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 
